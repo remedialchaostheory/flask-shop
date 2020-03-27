3fad96dbf3e56375fb70c1fd895a6b32c5ceb2f2
remedialchaostheory <remedialchaostheory@users.noreply.github.com>
2020-03-27 11:34:27 -0700

    Add crud test for StoreModel

--- tests/integration/models/test_store.py
+++ tests/integration/models/test_store.py
@@ -9,3 +9,23 @@ class StoreTest(IntegrationBaseTest):
         self.assertListEqual(
             store.items.all(), [], 'Store items length was not 0 when created.'
         )
+
+    def test_crud(self):
+        with self.app_context():
+            store_name = 'Test Store'
+            store = StoreModel(store_name)
+
+            self.assertIsNone(
+                StoreModel.find_by_name(store_name),
+                'Store model should be empty upon creation'
+            )
+            store.save_to_db()
+            self.assertIsNotNone(
+                StoreModel.find_by_name(store_name),
+                f'Did not find {store_name}'
+            )
+            store.delete_from_db()
+            self.assertIsNone(
+                StoreModel.find_by_name(store_name),
+                f'The store named "{store_name}" is still present when it should have been deleted'
+            )
