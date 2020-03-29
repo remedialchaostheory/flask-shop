fa319b074d5241d8437b77df7ad455466f2ba87f
remedialchaostheory <remedialchaostheory@users.noreply.github.com>
2020-03-29 13:52:24 -0700

    Import authentication with JWT and respective error handling

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import os
 
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Api
+from flask_jwt import JWT, JWTError
 
+from security import authenticate, identity
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 from resources.user import UserRegister
@@ -15,13 +17,25 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
     'DATABASE_URL', 'sqlite:///data.db'
 )
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'jdnalwih103uxnalq'
 api = Api(app)
+
+jwt = JWT(app, authenticate, identity)  # /auth
 
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
+
+
+@app.errorhandler(JWTError)
+def auth_error_handler(err):
+    return jsonify({
+        'message':
+        'Could not authorize. Did you include a valid Authorization header?'
+    }), 401
+
 
 if __name__ == '__main__':
     from db import db
