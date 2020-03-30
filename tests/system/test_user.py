9a2c54ea6e6c5b25207890a1ff749229c94858eb
remedialchaostheory <remedialchaostheory@users.noreply.github.com>
2020-03-30 08:05:18 -0700

    Add system test for duplicate users

--- tests/system/test_user.py
+++ tests/system/test_user.py
@@ -40,3 +40,28 @@ class UserTest(BaseTest):
                     headers={'Content-Type': 'application/json'}
                 )
                 self.assertIn('access_token', json.loads(auth_req.data).keys())
+
+    def test_register_duplicate_user(self):
+        username = 'test username'
+        password = 'test password'
+        with self.app() as client:
+            with self.app_context():
+                client.post(
+                    '/register',
+                    data={
+                        'username': username,
+                        'password': password
+                    }
+                )
+                resp = client.post(
+                    '/register',
+                    data={
+                        'username': username,
+                        'password': password
+                    }
+                )
+                self.assertEqual(resp.status_code, 400)
+                self.assertDictEqual({
+                    'message':
+                    'A user with that username already exists'
+                }, json.loads(resp.data))
