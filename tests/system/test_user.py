978944029bb12d084d7e7a3a65292f0966989ca5
remedialchaostheory <remedialchaostheory@users.noreply.github.com>
2020-03-30 08:04:44 -0700

    Add system test to register new user and login

--- tests/system/test_user.py
+++ tests/system/test_user.py
@@ -18,3 +18,25 @@ class UserTest(BaseTest):
                 self.assertIsNotNone(UserModel.find_by_username('test'))
                 self.assertDictEqual({'message': 'User created successfully.'},
                                      json.loads(resp.data))
+
+    def test_register_and_login(self):
+        username = 'test username'
+        password = 'test password'
+        with self.app() as client:
+            with self.app_context():
+                resp = client.post(
+                    '/register',
+                    data={
+                        'username': username,
+                        'password': password
+                    }
+                )
+                auth_req = client.post(
+                    '/auth',
+                    data=json.dumps({
+                        'username': username,
+                        'password': password
+                    }),
+                    headers={'Content-Type': 'application/json'}
+                )
+                self.assertIn('access_token', json.loads(auth_req.data).keys())
