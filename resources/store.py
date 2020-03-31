fdd70cce1567cf9970e8ada40385585b20b1f818
remedialchaostheory <remedialchaostheory@users.noreply.github.com>
2020-03-30 20:23:45 -0700

    Add store not found message when trying to delete a store that doesn't exist

--- resources/store.py
+++ resources/store.py
@@ -11,7 +11,10 @@ class Store(Resource):
 
     def post(self, name):
         if StoreModel.find_by_name(name):
-            return {'message': "A store with name '{}' already exists.".format(name)}, 400
+            return {
+                'message':
+                "A store with name '{}' already exists.".format(name)
+            }, 400
 
         store = StoreModel(name)
         try:
@@ -25,8 +28,9 @@ class Store(Resource):
         store = StoreModel.find_by_name(name)
         if store:
             store.delete_from_db()
+            return {'message': 'Store deleted'}
 
-        return {'message': 'Store deleted'}
+        return {'message': 'Store not found'}, 404
 
 
 class StoreList(Resource):
