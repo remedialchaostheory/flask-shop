8b84c6eebb9ac83a000e642a51ee0079f0f0d333
remedialchaostheory <remedialchaostheory@users.noreply.github.com>
2020-03-30 20:24:54 -0700

    Prevent erroneous msgs during dev testing by adding 'DEBUG' = False

--- tests/base_test.py
+++ tests/base_test.py
@@ -14,6 +14,7 @@ class BaseTest(TestCase):
     @classmethod
     def setUpClass(cls) -> None:
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'
+        app.config['DEBUG'] = False
         with app.app_context():
             db.init_app(app)
 
